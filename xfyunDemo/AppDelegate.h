e59335a2ed5b9697fa79e632b39ce84559cdc7b3
zzy <zzy@zzy.local>
2017-04-10 15:35:42 +0800

    Initial Commit

---
+++ xfyunDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xfyunDemo
+//
+//  Created by zzy on 2017/4/10.
+//  Copyright © 2017年 zzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xfyunDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xfyunDemo
+//
+//  Created by zzy on 2017/4/10.
+//  Copyright © 2017年 zzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
